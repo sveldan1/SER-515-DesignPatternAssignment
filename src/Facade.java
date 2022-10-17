2bd68e1ced8a987b55d94dcf0695c6058f3c8dd4
sveldan1 <sveldan1@asu.edu>
2022-10-16 20:15:44 -0700

    updated login

--- src/Facade.java
+++ src/Facade.java
@@ -1,3 +1,5 @@
+import java.io.*;
+import java.util.*;
 public class Facade {
 
 	private int UserType;
@@ -10,8 +12,93 @@ public class Facade {
 
 	private Person thePerson;
 
-	public boolean login() {
-		return false;
+	public boolean login() throws IOException {
+		Scanner scanner = new Scanner(new InputStreamReader(System.in));
+		System.out.println("Reading input from console");
+		System.out.println("Please enter username: ");
+		String username = scanner.nextLine();
+		System.out.println("Please enter password: ");
+		String password = scanner.nextLine();
+
+		File buyerfile = new File("src/BuyerInfo.txt");
+		BufferedReader bbr = new BufferedReader(new FileReader(buyerfile));
+
+		String bt;
+		final Map<String, String> buyermap = new HashMap<>();
+		while((bt=bbr.readLine())!=null) {
+			String[] Arr=bt.split(":");
+			buyermap.put(Arr[0], Arr[1]);
+
+		}
+		//System.out.println(buyermap);
+
+		File sellerfile=new File("src/SellerInfo.txt");
+		BufferedReader sbr = new BufferedReader(new FileReader(sellerfile));
+
+		String st;
+		final Map<String, String> sellermap = new HashMap<>();
+		while((st=sbr.readLine())!=null) {
+			String[] krr=st.split(":");
+			sellermap.put(krr[0], krr[1]);
+
+		}
+		//System.out.println(sellermap);
+
+		if(buyermap.containsKey(username)){
+			if((buyermap.get(username)).equals(password)){
+				System.out.println("user is a buyer");
+				return true;
+			}
+			else{
+				System.out.println("Password does not match");
+				return false;
+			}
+		}
+		else if(sellermap.containsKey(username)){
+			if((sellermap.get(username)).equals(password)){
+				System.out.println("user is a seller");
+				return true;
+			}
+			else{
+				System.out.println("password does not match");
+				return false;
+			}
+		}
+		else{
+			return false;
+		}
+
+//		for (String user: buyermap.keySet()){
+//			if(username.equals(user) && password.equals(buyermap.get(user))){
+//			System.out.println("user is a buyer");
+//			break;
+//			}
+//			else if(username.equals(user) && !password.equals(buyermap.get(user))){
+//				System.out.println("password does not match");
+//			}
+//			else{
+//				System.out.println("user is not a buyer");
+//			}
+//		}
+//		for (String user: sellermap.keySet()){
+//			if(username.equals(user) && password.equals(sellermap.get(user))){
+//				System.out.println("user is a seller");
+//				break;
+//			} else if (username.equals(user)&& !password.equals(sellermap.get(user))) {
+//				System.out.println("password does not match");
+//			} else{
+//				System.out.println("user is not a seller");
+//			}
+//		}
+
+
+
+
+
+			//System.out.println(St);
+
+
+		//return false;
 	}
 
 	public void addTrading() {
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         Facade facade=new Facade();
+        try {
+            if(facade.login()){
+            System.out.println("Welcome!");
+            }
+            else{
+                System.out.println("Check again");
+            }
+        } catch (IOException e) {
+            System.out.println("Unable to read");
+        }
 
-
-        System.out.println("Hello world!");
+        //System.out.println("Hello world!");
     }
 }
